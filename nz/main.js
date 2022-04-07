1c4e1928c9c74b8d73c0822eee45e140cfa307ae
Richard Matalewski <richard.matalewski@student.uibk.ac.at>
2022-04-07 15:43:50 +0200

    Zahlen durch Buchstaben ausgetauscht

--- nz/main.js
+++ nz/main.js
@@ -1,11 +1,14 @@
 /*Neuseelandreise*/
 //einzeiliger Kommentar
-var map = L.map('map').setView([-44.004674, 170.477121], 10);
+let lat = -44.004674;
+let lgn = 170.477121;
+let zoom = 10;
+let map = L.map('map').setView([lat, lgn], zoom);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([-44.004674, 170.477121]).addTo(map)
+L.marker([lat, long]).addTo(map)
     .bindPopup('<h3> Lake Tekapo </h3>')
     .openPopup();
