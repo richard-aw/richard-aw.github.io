40fc39af05a846e7c7e72f4f497c0c6f96afcf99
Richard Matalewski <richard.matalewski@student.uibk.ac.at>
2022-04-07 17:20:22 +0200

    Popup

--- nz/main.js
+++ nz/main.js
@@ -31,17 +31,21 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-let popup = 
-`<h3> ${ETAPPEN[0].titel} (Etappe ${ETAPPEN[0].nr})</h3>
-<ul>
-<li>geogr. Länge:${ETAPPEN[0].lng}</li>
-<li>geogr. Breite:${ETAPPEN[0].lat}</li>
-<li><a href="${ETAPPEN[0].wikipedia}">Link zur Wikipediaseite</a></li>
-<li><a href="${ETAPPEN[0].github}">Link zur Etappenseite</a></li>
-</ul>`;
+
 
 L.marker([lat, lng]).addTo(map)
-    .bindPopup(popup)
-    .openPopup();
 
-   
+
+   for(let etappe of ETAPPEN) {
+       let popup = 
+    `<h3> ${etappe.titel} (Etappe ${etappe.nr})</h3>
+    <ul>
+    <li>geogr. Länge:${etappe.lng}</li>
+    <li>geogr. Breite:${etappe.lat}</li>
+    <li><a href="${etappe.wikipedia}">Link zur Wikipediaseite</a></li>
+    <li><a href="https://${etappe.github}.github.io/nz/">Link zur Etappenseite</a></li>
+    </ul>`;
+    //console.log(etappe);
+    L.marker([etappe.lat, etappe.lng]).addTo(map).bindPopup(popup);
+    
+   }
