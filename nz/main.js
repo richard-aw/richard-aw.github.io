ebda9cc1dd023ea9d548943ded737cbaeb2f6420
Richard Matalewski <richard.matalewski@student.uibk.ac.at>
2022-04-07 15:57:13 +0200

    coords

--- nz/main.js
+++ nz/main.js
@@ -3,8 +3,15 @@
 let lat = -44.004674;
 let lgn = 170.477121;
 let zoom = 10;
-let map = L.map('map').setView([lat, lgn], zoom);
 
+let coords = [-44.004674,170.477121];
+
+let map = L.map('map').setView(coords, zoom);
+console.log(coords);
+console.log(coords[0]);
+console.log(coords[1]);
+console.log(coords.length)
+let coords = [-44.004674,170.477121];
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
